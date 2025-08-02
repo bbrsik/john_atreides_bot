ccc449b24df14cf6b4ea9a7ae238afffeb1f5053
Denis Verkhoturov <sainte66@gmail.com>
2025-08-02 03:33:35 +0300

    basic bot configuration, .env configuration, logging setup

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+import logging
+
+from dotenv import load_dotenv
+
+load_dotenv()
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
